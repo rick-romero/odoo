8781de4d46b75c4148df206831f306e6f510ae70
Rick Romero <hromero@netbiis.com>
2016-05-23 02:35:55 -0400

    clone rep

---
+++ setup/env/clone-rep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+sudo git clone https://github.com/rick-romero/l10n-brazil --branch 8.0 --depth 1
+sudo git clone https://github.com/rick-romero/trust-addons --branch 8.0 --depth 1
+sudo git clone https://github.com/rick-romero/bank-statement-reconcile --branch 8.0 --depth 1
+sudo git clone https://github.com/rick-romero/bank-statement-import --branch 8.0 --depth 1
+sudo git clone https://github.com/rick-romero/server-tools --branch 8.0 --depth 1
+sudo git clone https://github.com/rick-romero/odoo-brazil-banking --branch 8.0 --depth 1
+sudo git clone https://github.com/rick-romero/odoo-brazil-eletronic-documents --branch 8.0 --depth 1
+sudo git clone https://github.com/rick-romero/account-payment --branch 8.0 --depth 1
+sudo git clone https://github.com/rick-romero/account-fiscal-rule --branch 8.0 --depth 1
+sudo git clone https://github.com/rick-romero/bank-payment --branch 8.0 --depth 1
